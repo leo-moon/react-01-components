1b58081e3a690b44090ef88e088b3e0d0b228f2d
Your Nameleo-moon <lvv0820@gmail.com>
2024-06-26 13:07:40 +0300

    Task 1. Add props and defaultProps

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,8 @@
-import s from './profile.module.css'
+import PropTypes from 'prop-types';
 
-const User = ({
+import s from './profile.module.css';
+
+const Profile = ({
   username = 'username',
   tag = 'tag',
   location = "Land, Eurth",
@@ -19,27 +21,51 @@ const User = ({
       </li>)
   }
   )
+
   return (
-      <div className={s.profile}>
+    <div className={s.profile}>
 
-        <div className={s.description}>
-          <img
-            src={avatar}
-            alt="User avatar"
-            className={s.avatar}
-          />
-          <p className={s.name}>{username}</p>
-          <p className={s.tag}>{tag}</p>
-          <p className={s.location}>{location}</p>
-        </div>
-
-        <ul className={s.stats}>
-          {elements}
-        </ul>
-
+      <div className={s.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          className={s.avatar}
+        />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
+
+      <ul className={s.stats}>
+        {elements}
+      </ul>
+
+    </div>
   )
 }
 
-export default User
+export default Profile
 
+Profile.defaultProps = {
+  username: 'username',
+  tag: 'tag',
+  location: "Land, Eurth",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 0,
+    views: 0,
+    likes: 0
+  }
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape({
+    followers: PropTypes.string,
+    views: PropTypes.string,
+    likes: PropTypes.string
+  }))
+}
